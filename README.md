da9b4f30e7f0e6e4ea45b96f159aef20c20b7fe7
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-08-17 21:50:33 -0400

    became non-argument tool

--- README.md
+++ README.md
@@ -1,66 +1,13 @@
 # Kusaki
-Kusaki is a tool to find reuploads of YouTube videos using the power of search engines.
+Kusaki is a console tool to find reuploads of YouTube videos using the power of search engines.
 
 ## Setup
 
 1. Download the latest release of Kusaki and [Node.js](https://nodejs.org/en/download/).
 2. Install the dependencies via ``npm install``.
-3. Run Kusaki via ``node kusaki --help``
+3. Run Kusaki via ``node kusaki``
 
 ## How it works
 
-First, it searches DuckDuckGo (in HTML version of the site) for the ID, specifies what kind of result (English video sites),
-scrapes the results and throws the results in a JSON file.
-
-## Example
-
-Terminal Input:
-
-```node kusaki -i 7pNezfD7CLw```
-
-Terminal Output:
-
-```
-
-kusaki - youtube archival tool
-===============================
-- searching for the entire internet for the id...
-
-- pushing to array... [result 1]
-- pushing to array... [result 2]
-- pushing to array... [result 3]
-- pushing to array... [result 4]
-- pushing to array... [result 5]
-
-- writing results to json file...
-
-[!] finished file `./json/data-7pNezfD7CLw.json`
-
-```
-
-File Output (pretty-ified):
-
-```json
-[
-    {
-        "linkTitle":"D'oh! Actually: A Special Simpsons Episode [Um ... - YouTube",
-        "url":"https://www.youtube.com/watch?v=7pNezfD7CLw"
-    },
-    {
-        "linkTitle":"collegehumor.com URL-Extractor Report",
-        "url":"https://www.pentesterking.com/tools/urlextractor/collegehumor.com"
-    },
-    {
-        "linkTitle":"D'oh! Actually: A Special Simpsons Episode [Um Actually ...",
-        "url":"https://www.transcriptdb.com/watch/7pNezfD7CLw"
-    },
-    {
-        "linkTitle":"CollegeHumor - rsssearchhub.com",
-        "url":"https://www.rsssearchhub.com/feed/c41274469df7851e534dcbec2a4cf8d8/collegehumor-picture-galleries"
-    },
-    {
-        "linkTitle":"CollegeHumor - US",
-        "url":"https://invidio.us/feed/channel/UCPDXXXJj9nax0fr0Wfc048g"
-    }
-]
-```
+This searches [Archive.org](https://archive.org) and [Peteyvid.com](https://peteyvid.com) for IDs and titles for
+the video in question
--- kusaki.js
+++ kusaki.js
@@ -1,116 +1,172 @@
-const args = process.argv.slice(2);
 const got = require("got");
 const ytsr = require("ytsr");
 const ytdl = require("ytdl-core");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const url = require("url");
-if (!args[0]) {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("run `node kusaki -help` to see the available commands");
-    console.log("");
-} else if (args[0] == "--help") {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("`-i` - look for video by ID");
-    console.log("`-title` - look for video by title (less accurate)");
-    console.log("");
-} else if (args[0] == "-i" && !args[1]) {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("error! this parameter requires an input");
-    console.log("");
-} else if (args[0] == "-i" && !ytdl.validateID(args[1])) {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("error! this parameter requires a valid ");
-    console.log("YouTube id.");
-    console.log("");
-} else if (args[0] == "-i" && ytdl.validateID(args[1])) {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("- searching for the entire internet for the id...");
-    console.log("");
-    if (!fs.existsSync("./json")) {
-        fs.mkdirSync("./json/");
+const chalk = require('chalk');
+const { Input } = require('enquirer');
+console.log("");
+console.log("kusaki - youtube archival tool");
+console.log("===============================");
+console.log("if you don't know certain video details,");
+console.log("just leave it blank!");
+console.log("");
+var i1 = new Input({"message": "YouTube ID"})
+i1.run().then(function(a) {
+    if (a.toString().substring(0, 4) == "http") {
+        if (ytdl.validateURL(a.toString())) {
+            var ans1 = ytdl.getURLVideoID(a.toString());
+        } else {
+            console.log(chalk.red("ERR! Must be an ID or URL."));
+            return;
+        }
+    } else {
+        if (ytdl.validateID(a)) {
+            var ans1 = a.toString();
+        } else {
+            console.log(chalk.red("ERR! Must be an ID or URL."));
+            return;
+        }
     }
-    got("https://html.duckduckgo.com/html/?q=" + args[1] + "&attr=video&kl=us-en").then(function(response) {
-        var ddg = [];
-        var $ = cheerio.load(response.body);
-        for (var c in $(".result .links_main h2 a")) {
-            if ($(".result .links_main h2 a")[c].children && $(".result .links_main h2 a")[c].children[0] && $(".result .links_main h2 a")[c].children[0].data !== "No  results.") {
-                    //^ checks if there url has a name
-                if ($(".result .links_main h2 a")[c].attribs && $(".result .links_main h2 a")[c].attribs.href) {
-                    var a = url.parse($(".result .links_main h2 a")[c].attribs.href, true).query.uddg;
-                    var d = parseInt(c) + 1;
-                    console.log("- pushing to array... [result " + d + "]");
-                    ddg.push({"linkTitle": $(".result .links_main h2 a")[c].children[0].data, "url": a})
+    var i2 = new Input({"message": "Video Title"})
+    i2.run().then(function(a) {
+        const ans2 = a.toString();
+        cls();
+        console.log(chalk.yellow("- formatting main string..."));
+        if (!ytdl.validateID(ans1)) {
+            console.log(chalk.red("ERR! The YouTube ID is not valid"));
+            return;
+        }
+        const data = {
+            id: ans1,
+            title: ans2
+        }
+        searchArchive(data);
+    })
+})
+
+function cls() {
+    console.clear();
+    console.log("");
+    console.log("kusaki - youtube archival tool");
+    console.log("===============================");
+}
+
+function searchArchive(string) {
+    cls();
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [formatted string]"));
+    console.log(chalk.yellowBright("- searching archive.org..."));
+    if (!string.title) {
+        got('https://archive.org/search.php?query="' + string.id + '"&and[]=mediatype%3A"movies"').then(function(response) {
+            var $ = cheerio.load(response.body);
+            let one = [];
+            for (var c in $("div .results div .C234 div a")) {
+                if ($("div .results div .C234 div a")[c].attribs && $("div .results div .C234 div a")[c].attribs.href) {
+                    if ($("div .results div .C234 div a")[c].attribs.href.includes(string.id)) {
+                        one.push("https://archive.org" + $("div .results div .C234 div a")[c].attribs.href)
+                    }
                 }
             }
-        }
-        console.log("");
-        console.log("- writing results to json file...");
-        console.log("");
-        var fn = "./json/data-" + args[1] + ".json"
-        if (fs.existsSync(fn)) {
-            fs.unlinkSync(fn)
-        }
-        fs.appendFileSync(fn, "");
-        fs.writeFileSync(fn, JSON.stringify(ddg));
-        console.log("[!] finished file `" + fn + "`");
-        console.log("");
-    })
-} else if (args[0] == "-title" && !args[1]) {
-    console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("error! this parameter requires an input ");
-    console.log("(video title).");
-    console.log("");
-} else if (args[0] == "-title" && args[1]) {
-    if (!fs.existsSync("./json")) {
-        fs.mkdirSync("./json/");
-    }
-    var q = "";
-    for (var i = 1; i < args.length; i++) {
-        q = q + args[i];
-    }
-    got("https://html.duckduckgo.com/html/?q=" + q + " reupload&attr=video&kl=us-en").then(function(response) {
-        var ddg = [];
-        var $ = cheerio.load(response.body);
-        for (var c in $(".result .links_main h2 a")) {
-            if ($(".result .links_main h2 a")[c].children && $(".result .links_main h2 a")[c].children[0] && $(".result .links_main h2 a")[c].children[0].data !== "No  results.") {
-                    //^ checks if there url has a name
-                if ($(".result .links_main h2 a")[c].attribs && $(".result .links_main h2 a")[c].attribs.href) {
-                    var a = url.parse($(".result .links_main h2 a")[c].attribs.href, true).query.uddg;
-                    var d = parseInt(c) + 1;
-                    console.log("- pushing to array... [result " + d + "]");
-                    ddg.push({"linkTitle": $(".result .links_main h2 a")[c].children[0].data, "url": a})
+            searchPetey(string, one);
+        })
+    } else {
+        got('https://archive.org/search.php?query=' + string.id + ' ' + string.title + '&and[]=mediatype%3A"movies"').then(function(response) {
+            var $ = cheerio.load(response.body);
+            let one = [];
+            for (var c in $("div .results div .C234 div a")) {
+                if ($("div .results div .C234 div a")[c].attribs && $("div .results div .C234 div a")[c].attribs.href) {
+                    if ($("div .results div .C234 div a")[c].attribs.href.includes(string.id)) {
+                        one.push("https://archive.org" + $("div .results div .C234 div a")[c].attribs.href)
+                    }
                 }
             }
-        }
-        console.log("");
-        console.log("- writing results to json file...");
-        console.log("");
-        var fn = "./json/data-" + args[1] + ".json"
-        if (fs.existsSync(fn)) {
-            fs.unlinkSync(fn)
-        }
-        fs.appendFileSync(fn, "");
-        fs.writeFileSync(fn, JSON.stringify(ddg));
-        console.log("[!] finished file `" + fn + "`");
-        console.log("");
-    })
-} else {
+            searchPetey(string, one);
+        })
+    }
+}
+
+function searchPetey(string, old) {
+    cls();
+    if (!old) {
+        var old = [];
+    }
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [formatted string]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced archive.org]"));
+    console.log(chalk.yellowBright("- searching peteyvid.com..."));
+    if (old.length == 1) {
+        var logtail = old.length + " video";
+        console.log(chalk.yellow("[info]") + " found " + chalk.greenBright(logtail));
+    } else if (old.length > 1) {
+        var logtail = old.length + " videos";
+        console.log(chalk.yellow("[info]") + " found " + chalk.greenBright(logtail));
+    } else {
+        var logtail = "0 videos";
+        console.log(chalk.yellow("[info]") + " found " + chalk.redBright(logtail));
+    }
+    if (string.title) {
+        got("https://www.peteyvid.com/index.php?q=" + string.title + '"' + string.id + '"').then(function(response) {
+            var $ = cheerio.load(response.body);
+            for (var c in $("ol li blockquote")) {
+                if ($("ol li blockquote")[c].attribs) {
+                    if ($("ol li blockquote")[c].attribs.cite !== undefined) {
+                        old.push($("ol li blockquote")[c].attribs.cite);
+                    }
+                }
+            }
+            searchDailymotion(string, old)
+        })
+    } else {
+        got("https://www.peteyvid.com/index.php?q=" + '"' + string.id + '"').then(function(response) {
+            var $ = cheerio.load(response.body);
+            for (var c in $("ol li blockquote")) {
+                if ($("ol li blockquote")[c].attribs) {
+                    if ($("ol li blockquote")[c].attribs.cite !== undefined) {
+                        old.push($("ol li blockquote")[c].attribs.cite);
+                    }
+                }
+            }
+            finish(string, old)
+        })
+    }
+}
+
+function searchDailymotion(string, old) {
+    // WIP
+    cls();
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [formatted string]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced archive.org]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced peteyvid.com]"));
+    console.log(chalk.yellowBright("- searching dailymotion..."));
+    //got("")
+    finish(string, old);
+}
+
+function finish(string, data) {
+    cls();
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [formatted string]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced archive.org]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced peteyvid.com]"));
+    // console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced dailymotion]"));
+    console.log(chalk.yellowBright("- writing to json..."));
+    var fn = "./json/data-" + string.id + ".json";
+    fs.writeFileSync(fn, JSON.stringify(data));
+    cls();
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [formatted string]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced archive.org]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced peteyvid.com]"));
+    // console.log(chalk.greenBright("✓") + chalk.blueBright(" [searced dailymotion]"));
+    console.log(chalk.greenBright("✓") + chalk.blueBright(" [wrote to " + fn + "]"));
     console.log("");
-    console.log("kusaki - youtube archival tool");
-    console.log("===============================");
-    console.log("invalid arguments");
+    if (data.length == 1) {
+        var logtail = data.length + " video";
+        console.log(chalk.yellow("[info]") + " found " + chalk.greenBright(logtail));
+    } else if (data.length > 1) {
+        var logtail = data.length + " videos";
+        console.log(chalk.yellow("[info]") + " found " + chalk.greenBright(logtail));
+    } else {
+        var logtail = "0 videos";
+        console.log(chalk.yellow("[info]") + " found " + chalk.redBright(logtail));
+    }
     console.log("");
 }
